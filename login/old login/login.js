9c467652413e239f0dd9921ac6000647a7305d1b
Pula, Crisdianthy L. <ty202500538@wmsu.edu.ph>
2026-05-15 11:16:51 +0700

    Delete login/old login directory

--- login/old login/login.js
+++
@@ -1,9 +0,0 @@
-function togglePassword() {
-const passwordInput = document.getElementById('password');
-
-if (passwordInput.type === 'password') {
-passwordInput.type = 'text';
-} else {
-passwordInput.type = 'password';
-}
-}
